23b4d08abcb72d6c7b123176b765fff9160ef5c1
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2023-01-12 16:23:46 +0100

    uploading initial data cleaning file

---
+++ data_cleaning.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('DataScientist.csv')
+
+#remove the index
+#salary estimate correction
+#company name text only
+#state field
+#age of company
+#parsing of job description (python)
+
+
